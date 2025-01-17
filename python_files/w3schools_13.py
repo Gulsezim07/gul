b60e653d3d962a86b1f89cdbdd2a5fb7fc6d2fd6
Gulsezim07 <g_bilimzhankyzy@kbtu.kz>
2025-01-18 02:24:05 +0500

    Modify Strings

---
+++ python_files/w3schools_13.py
@@ -0,0 +1,12 @@
+#Upper
+a = "Hello, World!"
+print(a.upper())
+
+#Lower
+#a = "Hello, World!"
+#print(a.lower())
+
+
+#Remove Whitespace
+#a = " Hello, World! "
+#print(a.strip()) # returns "Hello, World!"
